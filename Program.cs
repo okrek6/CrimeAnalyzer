ae6c750fbecee0176a8424323168614082103a38
Brendan Krekeler <36323683+okrek6@users.noreply.github.com>
2020-04-08 11:09:19 -0500

    Intial Commit

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace CrimeAnalyzer
     {
         static void Main(string[] args)
         {
-            if (args.Length != 2)
+            if (args.Length != 2) //if there isn't two arg inputs, it will exit the program.
             {
                 Console.WriteLine("Crime Analyzer <crime_csv_file_path> <report_file path>");
                 Environment.Exit(1);
